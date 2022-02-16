748bd6c5aaafed4972c86848475332f8cf5885fb
ebn646 <eric.nichols@US-NYC-C02ZF8PCLVDQ.globalservs.com>
2022-02-16 10:16:13 -0500

    added buttons to add cards to list

--- src/components/project/BoardColumn.tsx
+++ src/components/project/BoardColumn.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Droppable, Draggable } from 'react-beautiful-dnd'
 import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
 import Card from './Card';
 import Paper from '@mui/material/Paper';
+import {
+    Button,
+    TextField
+} from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import AddIcon from '@mui/icons-material/Add';
 import { styled } from '@mui/material/styles';
 
 const Title = styled(Paper)(({ theme }) => ({
@@ -15,6 +20,13 @@ const Title = styled(Paper)(({ theme }) => ({
 }));
 
 export default function Column({ column, tasks, index }) {
+    const [addCard, showAddCard] = useState(false);
+
+    function toggleAddCard() {
+        console.log('toggle add card')
+        showAddCard(!addCard)
+    }
+
     return (
         <Draggable draggableId={column._id} index={index}>
             {
@@ -23,7 +35,7 @@ export default function Column({ column, tasks, index }) {
                         {...provided.draggableProps}
                         sx={{ width: '100%', maxWidth: 360, marginLeft: 1 }}>
                         <div style={{ height: 'auto', background: '#ebecf0', padding: 10 }}>
-                            <p style={{padding: 10}} {...provided.dragHandleProps}>{column.name}</p>
+                            <p style={{ padding: 10 }} {...provided.dragHandleProps}>{column.name}</p>
                             {/* <p style={{ fontSize: 10 }}>{column._id}</p> */}
                             <Droppable droppableId={column._id} index={index} type="card">
                                 {
@@ -37,6 +49,22 @@ export default function Column({ column, tasks, index }) {
                                     )
                                 }
                             </Droppable>
+                            <div style={{ display: 'flex', flexDirection: 'column' }}>
+                                {
+                                    addCard ? (
+                                        <>
+                                            <TextField id="outlined-basic" label="Enter a title for this card..." variant="filled" />
+                                            <div style={{ display: 'flex', marginTop: 8}}>
+                                                <Button variant="contained">
+                                                    Add card
+                                                </Button>
+                                                <Button variant="text" endIcon={<CloseIcon />} onClick={toggleAddCard} />
+                                            </div>
+                                        </>
+
+                                    ) : <Button onClick={toggleAddCard} startIcon={<AddIcon />}>Add a card</Button>
+                                }
+                            </div>
                         </div>
                     </Box>
                 )
